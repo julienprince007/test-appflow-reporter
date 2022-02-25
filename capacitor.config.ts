fe0c1a7c772fcecb4566f99134a7e83790c227c0
RAZAFIMANDIMBY Mamy Henintsoa <razafimandimby.mamyhenintsoa@gmail.com>
2022-02-25 12:05:16 +0300

    remove cap init

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'reporter.sowellapp.com',
+  appName: 'Reporter',
+  webDir: 'dist/spa',
+  bundledWebRuntime: false
+};
+
+export default config;
