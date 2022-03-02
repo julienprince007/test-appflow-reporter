abceb317360f488c8a428693d5bda2e24bfdf6e3
RAZAFIMANDIMBY Mamy Henintsoa <razafimandimby.mamyhenintsoa@gmail.com>
2022-03-02 16:59:02 +0300

    fix script variables

--- script.sh
+++ script.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 CI_PLATFORM = ${CI_PLATFORM}
 
-if(CI_PLATFORM=android) then
+if($CI_PLATFORM=android) then
   yarn build:android
 fi
 
-if(CI_PLATFORM=ios) then
+if($CI_PLATFORM=ios) then
   yarn build:ios
 fi
 
 export FASTLANE_LANE_NAME=beta
+echo $FASTLANE_LANE_NAME
