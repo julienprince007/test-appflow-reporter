b5ab842139a2395ced5c22a6573f6b1ccf794610
RAZAFIMANDIMBY Mamy Henintsoa <razafimandimby.mamyhenintsoa@gmail.com>
2022-03-02 17:27:52 +0300

    bash

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-if[${CI_PLATFORM} = "android"] then
+if[$CI_PLATFORM == "android"] then
   yarn build:android
 fi
 
-if[${CI_PLATFORM} = "ios"] then
+if[$CI_PLATFORM == "ios"] then
   yarn build:ios
 fi
 
-export FASTLANE_LANE_NAME=beta
+env FASTLANE_LANE_NAME="beta"
